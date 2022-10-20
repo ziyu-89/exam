0cec5bc8edff1d042d1864a5fb8b129740668bf4
ziyu <4a7g0119@stust.edu.tw>
2022-10-20 13:52:01 +0800

    Initial Commit

---
+++ exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exam
+//
+//  Created by 黃子瑜 on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
